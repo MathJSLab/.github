263e10471fb4be9fba3c6a228a7b8674b301434c
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2024-11-26 00:50:54 -0300

    Initial Commit

--- script/helper/download-files.cjs
+++ script/helper/download-files.cjs
@@ -1,25 +1,13 @@
-
 const fs = require('node:fs');
 const path = require('node:path');
 const { spawn } = require('node:child_process');
 
-global.console.log(`Running ${__filename} ...`);
-const defaultConfigFile = 'download.config.json'
-if (process.argv.length < 3) {
-    process.argv.push(defaultConfigFile);
-}
-if (!path.isAbsolute(process.argv[2])) {
-    process.argv[2] = path.resolve(process.argv[2]);
-}
-global.console.warn(`Downloading files specified in ${process.argv[2]} ...`);
-const downloadConfig = {};
-try {
-    fs.accessSync(process.argv[2], fs.constants.R_OK);
-    Object.assign(downloadConfig, JSON.parse(fs.readFileSync(process.argv[2], 'utf-8')));
-} catch (err) {
-    err.message = `cannot load and parse configuration file at ${process.argv[2]}\n${err.message}`;
-    throw err;
-}
+/**
+ * Run curl using spawn.
+ * @param {*} src
+ * @param {*} dest
+ * @returns
+ */
 async function spawnCurl(src, dest) {
     return new Promise((resolve, reject) => {
         const curl = spawn('curl', ['-k', '-o', dest, src]);
@@ -34,13 +22,55 @@ async function spawnCurl(src, dest) {
         });
     });
 }
-Promise.all(downloadConfig.files.map(async (file) => {
-    if (!path.isAbsolute(file.dest)) {
-        file.dest = path.resolve(file.dest);
-    }
-    fs.mkdirSync(path.dirname(file.dest), { recursive: true });
-    return spawnCurl(file.src, file.dest);
-})).then(_result => {
-    global.console.warn(`Downloading files specified in ${process.argv[2]} done.`);
-    global.console.log(`Running ${__filename} done.\r\n`);
-});
+/* Parse command line and set configuration file path. */
+global.console.log(`Running ${__filename} ...`);
+const defaultConfigFile = 'download.config.json';
+const argConfigFilePos = process.argv.length > 2 && process.argv[2].trim() === 'clean' ? 3 : 2;
+let downloadConfigPath = process.argv.length > argConfigFilePos ? process.argv[argConfigFilePos] : defaultConfigFile;
+if (!path.isAbsolute(downloadConfigPath)) {
+    downloadConfigPath = path.resolve(downloadConfigPath);
+}
+/* Read and parse configuration file. */
+const downloadConfig = {};
+try {
+    fs.accessSync(downloadConfigPath, fs.constants.R_OK);
+    Object.assign(downloadConfig, JSON.parse(fs.readFileSync(downloadConfigPath, 'utf-8')));
+} catch (err) {
+    err.message = `cannot load and parse configuration file at ${downloadConfigPath}\n${err.message}`;
+    throw err;
+}
+if (argConfigFilePos === 3) {
+    /* Cleanup downloaded files. */
+    global.console.warn(`Removing downloaded files specified in ${downloadConfigPath} ...`);
+    Promise.all(
+        downloadConfig.files.map(async (file) => {
+            let filePath;
+            try {
+                filePath = path.resolve(file.dest);
+                global.console.log(`Removing ${filePath} ...`);
+                fs.unlinkSync(filePath);
+                global.console.log(`Removing ${filePath} done.`);
+            } catch {
+                global.console.log(`Cannot remove ${filePath} \nIgnoring...`);
+            }
+        }),
+    ).then((_result) => {
+        global.console.warn(`Removing downloaded files specified in ${downloadConfigPath} done.`);
+        global.console.log(`Running ${__filename} done.\r\n`);
+    });
+} else {
+    /* Download files. */
+    global.console.warn(`Downloading files specified in ${downloadConfigPath} ...`);
+    Promise.all(
+        downloadConfig.files.map(async (file) => {
+            if (!path.isAbsolute(file.dest)) {
+                file.dest = path.resolve(file.dest);
+            }
+            fs.mkdirSync(path.dirname(file.dest), { recursive: true });
+            return spawnCurl(file.src, file.dest);
+        }),
+    ).then((_result) => {
+        global.console.warn(`Downloading files specified in ${downloadConfigPath} done.`);
+        global.console.log(`Running ${__filename} done.\r\n`);
+    });
+}
