50ab78eaeee11045dc7e50570c1ba4f5e22a78f5
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2026-04-21 13:15:56 -0300

    chore(build): Update in version 0.0.1 on Tue, 21 Apr 2026 16:15:51 GMT

--- script/build/helper/EleventyUtil.js
+++ script/build/helper/EleventyUtil.js
@@ -1,6 +1,29 @@
 // @ts-ignore
 import Image from '@11ty/eleventy-img';
-class default_1 {
+export default class default_1 {
+    /**
+     * Default template extensions and their aliases. The first element of the array is the default extension and others are aliases.
+     */
+    static templateExtension = {
+        HTML: ['html', 'htm', 'xhtml', 'shtml'],
+        Markdown: ['md', 'markdown', 'txt', 'mkd', 'mdown'],
+        WebC: ['webc'],
+        JavaScript: ['11ty.js', 'js', 'cjs', 'mjs'],
+        JSON: ['json', 'jsonc', 'geojson', 'topojson'],
+        Liquid: ['liquid', 'liquify'],
+        Nunjucks: ['njk', 'nunj', 'nunjucks'],
+        Handlebars: ['hbs', 'handlebars', 'hbs.html'],
+        Mustache: ['mustache', 'mustach'],
+        EJS: ['ejs', 'ejs.html'],
+        Haml: ['haml', 'hamlc'],
+        Pug: ['pug', 'jade'],
+        TypeScript: ['ts', 'tsx'],
+        JSX: ['jsx', 'jsx.js'],
+        MDX: ['mdx', 'mdjsx'],
+        CoffeeScript: ['coffee', 'cs', 'cjsx'],
+        CSON: ['cson', 'csn'],
+        SASS: ['scss', 'sass'],
+    };
     static async transformImage(images, options) {
         if (!Array.isArray(images)) {
             images = [images];
@@ -35,27 +58,3 @@ class default_1 {
         });
     }
 }
-/**
- * Default template extensions and their aliases. The first element of the array is the default extension and others are aliases.
- */
-default_1.templateExtension = {
-    HTML: ['html', 'htm', 'xhtml', 'shtml'],
-    Markdown: ['md', 'markdown', 'txt', 'mkd', 'mdown'],
-    WebC: ['webc'],
-    JavaScript: ['11ty.js', 'js', 'cjs', 'mjs'],
-    JSON: ['json', 'jsonc', 'geojson', 'topojson'],
-    Liquid: ['liquid', 'liquify'],
-    Nunjucks: ['njk', 'nunj', 'nunjucks'],
-    Handlebars: ['hbs', 'handlebars', 'hbs.html'],
-    Mustache: ['mustache', 'mustach'],
-    EJS: ['ejs', 'ejs.html'],
-    Haml: ['haml', 'hamlc'],
-    Pug: ['pug', 'jade'],
-    TypeScript: ['ts', 'tsx'],
-    JSX: ['jsx', 'jsx.js'],
-    MDX: ['mdx', 'mdjsx'],
-    CoffeeScript: ['coffee', 'cs', 'cjsx'],
-    CSON: ['cson', 'csn'],
-    SASS: ['scss', 'sass'],
-};
-export default default_1;
--- test/data/date-time-test.md
+++ test/data/date-time-test.md
@@ -3,5 +3,5 @@
 This file was generated in**21/04/2026**.
 
 - 21/04/2026
-- I117O
+- I849O
 - 2026-04-21
